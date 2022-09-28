ec3610a466dc28d76575e170005dcb6e1b6e371c
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-27 23:39:28 -0300

    up

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
+import happyEmoji from '@assets/happy.png'
+
+import { Search } from '@components/Search'
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText
+} from './style'
+
+export function Home() {
+  const { COLORS } = useTheme()
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá,Admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+      <Search onSearch={() => {}} onClear={() => {}} />
+    </Container>
+  )
+}
